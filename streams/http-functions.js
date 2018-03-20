4f819c847ec8b3f85c844db9504fad84f2e53fdd
Owen Young Soo Koh <kohys92@gmail.com>
2018-03-20 01:06:08 +0000

    Make it module

---
+++ streams/http-functions.js
@@ -0,0 +1,18 @@
+
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+
+  https.get(options, function(response){
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      callback(data);
+    });
+
+    response.on('end', function(){
+
+    });
+  });
+}
+
